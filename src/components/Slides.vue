0ebfe2d113b27ae6ef036ffe0a97cafe0798ca99
Julian Cruz Sanchez <kickbutt.js@gmail.com>
2023-03-24 12:29:25 -0600

    Added Content

---
+++ src/components/Slides.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="slides" slide="1">
+    <div :class="`slide ${$props.current == 1 ? 'current' : ''} center-div`">
+      <img class="ios" src="../assets/iphone.png" />
+      <img class="android" src="../assets/android.png" />
+    </div>
+    <div :class="`slide p-5 center-div max-w-[50vh] ${$props.current == 2 ? 'current' : ''}`">
+      <div><h1 class="text-3xl font-bold">Our Mission</h1>
+        <br>
+      <!-- <p>
+        The Bee is a revolutionary social media app designed to offer users complete freedom and control over their online
+        experience. Unlike other social media platforms that impose strict rules and filters, The Bee allows users to pick
+        their own filters and create a customized, tailored experience that suits their unique needs and preferences.
+        <br><br>
+        With The Hive, users can choose from a wide range of filters to create the perfect environment for their social
+        media activity. Whether you're looking to create a minimalist, distraction-free feed, or want an open view of the
+        world, The Hive has something to offer for everyone.
+        <br><br>
+        The Hive is more than just a social media app - it's a community where individuals can come together to share
+        their experiences, opinions, and perspectives in a way that is free from judgement and censorship. Whether you're
+        looking to connect with old friends or make new ones, The Hive is the perfect place to join the conversation and
+        be a part of something special.
+      </p> -->
+      <p>
+      <ul>
+        <li> The Bee is a social media network focused on promoting freedom of expression and individual choice.
+        </li><br>
+        <li>Users have the ability to choose their own content filters, allowing them to curate their own experience on
+          the
+          platform.</li><br>
+        <li>The platform prioritizes user privacy and security, with robust measures in place to protect user data and
+          prevent
+          censorship.</li><br>
+        <li>The Bee fosters a community of open-minded individuals who value free speech and diverse perspectives.</li>
+        <br>
+        <li>Users can share a variety of content, including text, images, videos, and links, without fear of being
+          censored or
+          restricted.</li><br>
+      </ul>
+      </p>
+    </div>
+      
+    </div>
+    <div :class="`slide center-div ${$props.current == 3 ? 'current' : ''}`">
+      <div class="grid gap-5 p-5">
+        <h1 class="text-3xl">Thank You for Signing Up!!</h1>
+        <button class="button" @click="$emit('donate', 1.00)">Donate $1.00</button>
+        <button class="button" @click="$emit('donate', 3.00)">Donate $3.00</button>
+        <button class="button" @click="$emit('donate', 5.00)">Donate $5.00</button>
+        <div class="subscribe-wrapper">
+          <input class="input" v-model="donateAmount" step="0.01" type=" number" placeholder="Donate More" />
+          <button @click="$emit('donate', donateAmount)" class="button">Donate</button>
+        </div>
+      </div>
+    </div>
+    <div :class="`slide center-div ${$props.current == 4 ? 'current' : ''}`">
+      <h1 class="text-3xl">Thank You for Your Donation!!</h1>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  props: ["current"],
+  data() {
+    return {
+      donateAmount: ""
+    }
+  }
+}
+</script>
+<style lang="scss">
+.slides {
+  background: rgba(255, 205, 0, 0.6705882353);
+  display: grid;
+}
+
+.slide img {
+  max-height: 100%;
+}
+
+.slide {
+  max-height: calc(100vh - 130px - 54px - 24px);
+  transition: all 0.5s ease-in-out;
+  opacity: 0;
+  grid-column: 1;
+  grid-row: 1;
+}
+
+.slide.current {
+  opacity: 1;
+  z-index: 1000;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to
+
+/* .fade-leave-active below version 2.1.8 */
+  {
+  opacity: 0;
+}
+
+@supports (-webkit-touch-callout: none) {
+  .android {
+    display: none;
+  }
+}
+
+@supports not (-webkit-touch-callout: none) {
+  .ios {
+    display: none;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,212 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <div class="topbar" @click="currentSlide = 1"></div>
+    <!-- Desktop-->
+    <div class="desktop-phone center-div" style="transform: translateY(-150px) scale(95%);">
+      <div class="grid gap-5 slide-container">
+        <Slides :current="currentSlide" @donate="amt => donate(amt)"></Slides>
+        <div>
+          <div class="subscribe-wrapper" v-if="currentSlide == 1 || currentSlide == 2">
+            <input class="input" type="text" placeholder="Enter email address" autocomplete="off" v-model="email"
+              @click="() => { currentSlide = 2; document.body.scrollTop = document.documentElement.scrollTop = 0; }" />
+            <button class="button" @click="getNotified()">Get Notified</button>
+          </div>
+          <span v-if="currentSlide == 1 || currentSlide == 2">By pressing get notified you agree to our
+            <a class="font-bold" href="/#/legal">Terms and Agreements</a>
+          </span>
+        </div>
+
+      </div>
+    </div>
+    <div class="sidetext-wrapper">
+      <div class="sidetext">
+        Your Hive.<br>
+        Your Choice.<br>
+        Your Way.<br>
+      </div>
+    </div>
+
+    <!-- Mobile -->
+
+    <div class="center-div mobile-steps">
+      <Slides :current="currentSlide" @donate="amt => donate(amt)"></Slides>
+    </div>
+    <div class="grid center-div mobile-signup-wrapper mt-5 mx-5" v-if="currentSlide == 1 || currentSlide == 2">
+      <input class="input" type="text" placeholder="Enter email address" autocomplete="off" v-model="email"
+        @focus="currentSlide = 2" />
+      <button class="button" @click="getNotified()">Get Notified</button>
+    </div>
+
+    <span class="center-div" style="font-size: 12px" v-if="currentSlide == 1 || currentSlide == 2">By pressing get
+      notified you agree to our
+      &nbsp;
+      <a class="font-bold" href="/#/legal">Terms and Agreements</a>
+    </span>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Slides from '../components/Slides.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Slides
+  },
+  data() {
+    return {
+      currentSlide: 1,
+      email: ""
+    }
+  },
+  methods: {
+    getNotified() {
+      var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+      if (this.$data.email !== "" && this.$data.email.match(validRegex)) {
+        this.$data.currentSlide = 3
+      } else {
+        alert("Invalid Email! Please try again.")
+        this.$data.email = ""
+      }
+    },
+    donate(amt) {
+      console.log(amt)
+      this.$data.currentSlide = 4
+    }
   }
 }
 </script>
+
+<style lang="scss">
+html {
+  background-image: url(../assets/hive.png);
+  background-position: top right;
+  background-repeat: no-repeat;
+  background-color: #FFCD00;
+}
+
+.topbar {
+  background: url(../assets/logo.png), #ffcd00ab;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 100px;
+  padding: 15px;
+
+}
+
+.subscribe-wrapper {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 10px;
+  font-size: 24px;
+}
+
+.input {
+  background-color: #FFE98E;
+  padding: 15px;
+  width: 100%;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px #00000023;
+}
+
+.button {
+  color: #FFCD00;
+  background: #000;
+  border-radius: 8px;
+  padding: 15px;
+}
+
+.sidetext-wrapper {
+  display: none;
+  position: fixed;
+  top: 50%;
+  transform: translate(20px, -50%);
+  font-size: 24px;
+}
+
+.sidetext {
+  font-size: 70px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.desktop-phone {
+  display: none;
+}
+
+.phone-mobile {
+  height: 75vh;
+  margin-top: 0.625rem;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.motto {
+  font-size: 40px;
+  font-weight: bold;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  text-align: center;
+}
+
+.mobile-signup-wrapper {
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 10px;
+}
+
+.slide-container {
+  height: 100vh;
+  grid-template-rows: 1fr auto;
+}
+
+@media (min-width: 1262px) {
+
+  html,
+  body {
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  .topbar {
+    background-image: url(../assets/logo-cs.png);
+    background-color: #00000000;
+    background-position: top left;
+    margin: 10px;
+    height: 150px;
+  }
+
+  .sidetext-wrapper {
+    display: block;
+  }
+
+  .desktop-phone {
+    display: flex;
+  }
+
+  .phone-mobile {
+    display: none;
+  }
+
+  .mobile-signup-wrapper {
+    display: none !important;
+  }
+
+  .slide {
+    height: auto
+  }
+
+  .mobile-steps {
+    display: none;
+  }
+}
+
+.keyboardOpen {
+  color: red;
+}
+</style>
